a22dd4d693fccee5b96b95cffadcf2c5b9b76c7e
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2024-02-07 13:51:08 +0100

    Initial Commit

---
+++ Orchard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orchard
+//
+//  Created by Johan Karlsson on 2024-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orchard/OrchardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrchardApp.swift
+//  Orchard
+//
+//  Created by Johan Karlsson on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct OrchardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
